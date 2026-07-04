24bbcaa89066bfe684bababd540dc1afb644c11c
Shivangi1515 <shivangichaurasia1515@gmail.com>
2026-07-04 16:12:48 +0530

    feat(pricing): integrate hero and brokerage components in pricing page

--- frontend/src/landing_page/pricing/PricingPage.js
+++ frontend/src/landing_page/pricing/PricingPage.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import Hero from '../home/Hero';
+import Hero from './Hero';
 import Brokerage from './Brokerage';
 import OpenAccount from '../OpenAccount';
 
 function PricingPage() {
     return ( 
         <>
-        <h1>Pricing</h1>
         <Hero/>
         <OpenAccount/>
         <Brokerage/>
