ac194619bb6c6e33f70bcdcd0f2a4299ca893459
Shivangi1515 <shivangichaurasia1515@gmail.com>
2026-07-04 16:12:51 +0530

    feat(products): implement RightSection component layout

--- frontend/src/landing_page/products/RightSection.js
+++ frontend/src/landing_page/products/RightSection.js
@@ -1,12 +1,53 @@
 import React from 'react';
 
-function RightSection() {
-    return ( 
-        <div>
-            <h1>Right Section</h1>
-            <p>This is the right section of the products page.</p>
+function RightSection(
+  {
+    imageURL,
+    productName,
+    productDescription,
+    learnMore
+  }
+) {
+  return (
+    <div className="container py-5">
+      <div className="row align-items-center">
+
+        {/* Left Side Image */}
+
+
+        {/* Right Side Content */}
+        <div className="col-lg-6 mt-5 mt-lg-0">
+
+          <h1>{productName}</h1>
+
+          <p className="text-muted mt-3">
+            {productDescription}
+          </p>
+
+          <div className="mt-4">
+
+            <a
+              href={learnMore}
+              className="text-decoration-none"
+            >
+              Learn More →
+            </a>
+          </div>
+
+
+
         </div>
-     );
+        <div className="col-lg-6 text-center">
+          <img
+            src={imageURL}
+            alt={productName}
+            className="img-fluid"
+          />
+        </div>
+
+      </div>
+    </div>
+  );
 }
 
 export default RightSection;
